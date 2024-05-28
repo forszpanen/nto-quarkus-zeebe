f177e58b3acfbfcc66f3d43d14dabcebdac81ce3
Marcin <marcin.a.stankiewicz@capgemini.com>
2024-05-28 14:54:13 +0200

    feat: add zeebe client enable property adjusted for Quarkus 3

--- runtime/src/main/java/io/quarkiverse/zeebe/runtime/health/ZeebeTopologyHealthCheck.java
+++ runtime/src/main/java/io/quarkiverse/zeebe/runtime/health/ZeebeTopologyHealthCheck.java
@@ -2,6 +2,7 @@ package io.quarkiverse.zeebe.runtime.health;
 
 import jakarta.inject.Inject;
 
+import org.eclipse.microprofile.config.ConfigProvider;
 import org.eclipse.microprofile.health.HealthCheck;
 import org.eclipse.microprofile.health.HealthCheckResponse;
 import org.eclipse.microprofile.health.HealthCheckResponseBuilder;
@@ -10,9 +11,12 @@ import org.eclipse.microprofile.health.Liveness;
 import io.camunda.zeebe.client.ZeebeClient;
 import io.camunda.zeebe.client.api.command.ClientStatusException;
 import io.camunda.zeebe.client.api.response.Topology;
+import io.quarkus.runtime.configuration.ConfigUtils;
 
 @Liveness
 public class ZeebeTopologyHealthCheck implements HealthCheck {
+
+    private static final String PROP_ZEEBE_ENABLED = "camunda.client.zeebe.enabled";
 
     @Inject
     ZeebeClient client;
@@ -20,19 +24,24 @@ public class ZeebeTopologyHealthCheck implements HealthCheck {
     @Override
     public HealthCheckResponse call() {
         HealthCheckResponseBuilder builder = HealthCheckResponse.named("Zeebe topology health check");
-        try {
-            Topology topology = client.newTopologyRequest().send().join();
-            if (topology.getClusterSize() > 0) {
-                return builder.up().build();
+        if (!ConfigUtils.isPropertyPresent(PROP_ZEEBE_ENABLED) ||
+                ConfigProvider.getConfig().getConfigValue(PROP_ZEEBE_ENABLED).getValue().equals("true")) {
+            try {
+                Topology topology = client.newTopologyRequest().send().join();
+                if (topology.getClusterSize() > 0) {
+                    return builder.up().build();
+                }
+                return builder.down().withData("reason", "No brokers found").build();
+            } catch (ClientStatusException e) {
+                return builder.down()
+                        .withData("reason", e.getMessage())
+                        .withData("code", e.getStatusCode().value())
+                        .build();
+            } catch (Exception ex) {
+                return builder.down().withData("reason", ex.getMessage()).build();
             }
-            return builder.down().withData("reason", "No brokers found").build();
-        } catch (ClientStatusException e) {
-            return builder.down()
-                    .withData("reason", e.getMessage())
-                    .withData("code", e.getStatusCode().value())
-                    .build();
-        } catch (Exception ex) {
-            return builder.down().withData("reason", ex.getMessage()).build();
+        } else {
+            return builder.up().build();
         }
     }
 }
